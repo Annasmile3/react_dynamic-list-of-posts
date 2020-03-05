9db66d597df24a63d23990abae97e650bd0a1643
Anna <anna.lyrynenko@gmail.com>
2020-03-05 16:56:00 +0200

    Updates

---
+++ precache-manifest.b77ac4d1e6c7b8ebcd397c9538aa495e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ca6a3d4c19c4536deca95210da8cad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5291eae372f7ea04274",
+    "url": "./static/css/main.8f37ff87.chunk.css"
+  },
+  {
+    "revision": "fc8edd6036c2f045e64c",
+    "url": "./static/js/2.4ce7076b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.4ce7076b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5291eae372f7ea04274",
+    "url": "./static/js/main.fec58e32.chunk.js"
+  },
+  {
+    "revision": "acb15a54c7276c556e90",
+    "url": "./static/js/runtime-main.8f6a98c8.js"
+  }
+]);
